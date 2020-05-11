681ad019fd968ec449a393b32bedd084ae2c2b03
Idriss91130 <idriss.fall@hetic.net>
2020-05-11 14:49:10 +0200

    responsive about

--- dist/script.d573be0b.js
+++ dist/script.d573be0b.js
@@ -157,7 +157,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54533" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54660" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
